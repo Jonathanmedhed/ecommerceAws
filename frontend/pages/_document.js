7b4d3a22490c8441930bc388a5ddab1fa48d5611
Jonathan Medina <jonathanmedhed@gmail.com>
2021-01-18 14:07:17 -0400

    implemented google analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import getConfig from 'next/config'
+const { publicRuntimeConfig } = getConfig()
 
 class MyDocument extends Document {
+	setGoogleTags() {
+		if (publicRuntimeConfig.PRODUCTION) {
+			return {
+				__html: `
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+			  
+				gtag('config', 'G-HTRF2DDYX5');
+        `,
+			}
+		}
+	}
+
 	render() {
 		return (
 			<Html lang="en">
@@ -12,6 +28,8 @@ class MyDocument extends Document {
 						crossorigin="anonymous"
 					/>
 					<link rel="stylesheet" href="/static/css/styles.css" />
+					<script async src="https://www.googletagmanager.com/gtag/js?id=G-HTRF2DDYX5"></script>
+					<script dangerouslySetInnerHTML={this.setGoogleTags()} />
 				</Head>
 				<body>
 					<Main />
