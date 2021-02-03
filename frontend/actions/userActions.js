079f40dcfbce706030a0563cd172a9e56e54733d
Jonathan Medina <jonathanmedhed@gmail.com>
2021-02-03 08:58:11 -0400

    clear more cart info on logout

--- frontend/actions/userActions.js
+++ frontend/actions/userActions.js
@@ -27,6 +27,7 @@ import {
 	USER_UPDATE_REQUEST,
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET, ORDER_LIST_RESET, ORDER_VIEWED_SUCCESS } from '../constants/orderConstants'
+import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 import { API } from '../config'
 // utilities
 import { validateEmail } from '../utilities/utilities'
@@ -84,11 +85,16 @@ export const logout = () => (dispatch) => {
 	localStorage.removeItem('cartItems')
 	localStorage.removeItem('shippingAddress')
 	localStorage.removeItem('paymentMethod')
+	localStorage.removeItem('subTotal')
+	localStorage.removeItem('total')
+	localStorage.removeItem('discountTotal')
+	localStorage.removeItem('freeAmount')
 	dispatch({ type: USER_LOGOUT })
 	dispatch({ type: USER_DETAILS_RESET })
 	dispatch({ type: ORDER_LIST_MY_RESET })
 	dispatch({ type: ORDER_LIST_RESET })
 	dispatch({ type: USER_LIST_RESET })
+	dispatch({ type: CART_CLEAR_ITEMS })
 	dispatch(removeAlert())
 	document.location.href = '/login'
 }
